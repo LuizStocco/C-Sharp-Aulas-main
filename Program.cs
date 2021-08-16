c08136d3036f365abcfb47dfe1deb698d55abd27
Luiz Henrique Stocco <henrique.riot@hotmail.com>
2021-08-16 19:43:36 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_Sharp_Aulas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Versionamento de códigos!!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# C-Sharp-Aulas
+Aulas de C# 
+#Exemplo de utilização do GitHub (Interface do VS Code)
